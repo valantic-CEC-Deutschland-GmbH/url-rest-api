a13d482a1f9bf1787e7c67027d0b5fee06549a8d
Patrick Schönfeld <patrick.schoenfeld@cec.valantic.com>
2023-02-13 14:03:06 +0100

    no-task add tests

--- tests/ValanticSprykerTest/Glue/UrlsRestApi/UrlsRestApiFactoryTest.php
+++ tests/ValanticSprykerTest/Glue/UrlsRestApi/UrlsRestApiFactoryTest.php
@@ -2,7 +2,6 @@
 
 declare(strict_types = 1);
 
-
 namespace ValanticSprykerTest\Glue\UrlsRestApi;
 
 use Codeception\Test\Unit;
@@ -11,9 +10,9 @@ use Spryker\Client\UrlStorage\UrlStorageClient;
 use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface;
 use Spryker\Glue\Kernel\Container;
 use Spryker\Glue\UrlsRestApi\Dependency\Client\UrlsRestApiToUrlStorageClientInterface;
+use Spryker\Glue\UrlsRestApi\UrlsRestApiDependencyProvider as SprykerUrlsRestApiDependencyProvider;
 use ValanticSpryker\Glue\UrlsRestApi\Processor\Url\Resolver\UrlResolver;
 use ValanticSpryker\Glue\UrlsRestApi\UrlsRestApiDependencyProvider;
-use Spryker\Glue\UrlsRestApi\UrlsRestApiDependencyProvider as SprykerUrlsRestApiDependencyProvider;
 use ValanticSpryker\Glue\UrlsRestApi\UrlsRestApiFactory;
 
 /**
@@ -27,90 +26,92 @@ use ValanticSpryker\Glue\UrlsRestApi\UrlsRestApiFactory;
 class UrlsRestApiFactoryTest extends Unit
 {
   /**
-   * @var UrlsRestApiFactory
+   * @var \ValanticSpryker\Glue\UrlsRestApi\UrlsRestApiFactory
    */
-  protected $businessFactory;
-  /**
-   * @var MockObject|Container
-   */
-  private Container $containerMock;
-  /**
-   * @var MockObject|(UrlStorageClient&MockObject)
-   */
-  private MockObject|UrlStorageClient $urlStorageClient;
-  /**
-   * @var MockObject|(RestResourceBuilderInterface&MockObject)
-   */
-  private RestResourceBuilderInterface|MockObject $restResourceBuilderMock;
+    protected $businessFactory;
 
   /**
-   * @return void
+   * @var \PHPUnit\Framework\MockObject\MockObject|\Spryker\Glue\Kernel\Container
    */
-  protected function _before(): void
-  {
-    parent::_before();
-  }
+    private Container $containerMock;
+
+  /**
+   * @var \PHPUnit\Framework\MockObject\MockObject|(\Spryker\Client\UrlStorage\UrlStorageClient&\PHPUnit\Framework\MockObject\MockObject)
+   */
+    private MockObject|UrlStorageClient $urlStorageClient;
+
+  /**
+   * @var \PHPUnit\Framework\MockObject\MockObject|(\Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface&\PHPUnit\Framework\MockObject\MockObject)
+   */
+    private RestResourceBuilderInterface|MockObject $restResourceBuilderMock;
 
   /**
    * @return void
    */
-  public function testCreateUrlResolver(): void
-  {
-    $this->containerMock = $this->getMockBuilder(Container::class)
-      ->disableOriginalConstructor()
-      ->getMock();
+    protected function _before(): void
+    {
+        parent::_before();
+    }
 
-    $this->urlStorageClient = $this->getMockBuilder(UrlsRestApiToUrlStorageClientInterface::class)
-      ->disableOriginalConstructor()
-      ->getMock();
+  /**
+   * @return void
+   */
+    public function testCreateUrlResolver(): void
+    {
+        $this->containerMock = $this->getMockBuilder(Container::class)
+        ->disableOriginalConstructor()
+        ->getMock();
 
-    $this->containerMock->expects(static::atLeastOnce())
-      ->method('has')
-      ->willReturn(true);
+        $this->urlStorageClient = $this->getMockBuilder(UrlsRestApiToUrlStorageClientInterface::class)
+        ->disableOriginalConstructor()
+        ->getMock();
 
-    $this->containerMock->expects(static::atLeastOnce())
-      ->method('get')
-      ->withConsecutive(
-          [SprykerUrlsRestApiDependencyProvider::CLIENT_URL_STORAGE],
-          [SprykerUrlsRestApiDependencyProvider::PLUGINS_REST_URL_RESOLVER_ATTRIBUTES_TRANSFER_PROVIDER],
-          [UrlsRestApiDependencyProvider::URL_RESOLVER_PLUGINS]
-      )
-      ->willReturnOnConsecutiveCalls(
-        $this->urlStorageClient,
-        [],
-        [],
-      );
+        $this->containerMock->expects(static::atLeastOnce())
+        ->method('has')
+        ->willReturn(true);
 
+        $this->containerMock->expects(static::atLeastOnce())
+        ->method('get')
+        ->withConsecutive(
+            [SprykerUrlsRestApiDependencyProvider::CLIENT_URL_STORAGE],
+            [SprykerUrlsRestApiDependencyProvider::PLUGINS_REST_URL_RESOLVER_ATTRIBUTES_TRANSFER_PROVIDER],
+            [UrlsRestApiDependencyProvider::URL_RESOLVER_PLUGINS],
+        )
+        ->willReturnOnConsecutiveCalls(
+            $this->urlStorageClient,
+            [],
+            [],
+        );
 
-    $this->restResourceBuilderMock = $this->getMockBuilder(RestResourceBuilderInterface::class)
-      ->disableOriginalConstructor()
-      ->getMock();
+        $this->restResourceBuilderMock = $this->getMockBuilder(RestResourceBuilderInterface::class)
+        ->disableOriginalConstructor()
+        ->getMock();
 
-    $this->businessFactory = new class ($this->restResourceBuilderMock) extends UrlsRestApiFactory {
-      /**
-       * @var \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
-       */
-      protected $restResourceBuilder;
+        $this->businessFactory = new class ($this->restResourceBuilderMock) extends UrlsRestApiFactory {
+          /**
+           * @var \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
+           */
+            protected RestResourceBuilderInterface $restResourceBuilder;
 
-      /**
-       * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface $restResourceBuilder
-       */
-      public function __construct(RestResourceBuilderInterface $restResourceBuilder)
-      {
-        $this->restResourceBuilder = $restResourceBuilder;
-      }
+          /**
+           * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface $restResourceBuilder
+           */
+            public function __construct(RestResourceBuilderInterface $restResourceBuilder)
+            {
+                $this->restResourceBuilder = $restResourceBuilder;
+            }
 
-      /**
-       * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
-       */
-      public function getResourceBuilder(): RestResourceBuilderInterface
-      {
-        return $this->restResourceBuilder;
-      }
-    };
+          /**
+           * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
+           */
+            public function getResourceBuilder(): RestResourceBuilderInterface
+            {
+                return $this->restResourceBuilder;
+            }
+        };
 
-    $this->businessFactory->setContainer($this->containerMock);
+        $this->businessFactory->setContainer($this->containerMock);
 
-    $this->assertInstanceOf(UrlResolver::class, $this->businessFactory->createUrlResolver());
-  }
+        $this->assertInstanceOf(UrlResolver::class, $this->businessFactory->createUrlResolver());
+    }
 }
